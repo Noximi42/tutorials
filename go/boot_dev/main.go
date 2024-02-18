9e7adc9f5bc4245738b322cd7baf087ae020189e
Christoph <christoph.biernoth@outlook.com>
2024-02-18 17:00:41 +0100

    Added feeds

--- go/boot_dev/main.go
+++ go/boot_dev/main.go
@@ -5,69 +5,71 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"rssagg/internal/database"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 
-  _ "github.com/lib/pq"
+	"rssagg/internal/database"
 )
 
 type apiConfig struct {
-  DB *database.Queries
+	DB *database.Queries
 }
 
 func main() {
-  godotenv.Load(".env")
+	godotenv.Load(".env")
 
-  portString := os.Getenv("PORT")
-  if portString == "" {
-    log.Fatal("PORT is not found in the environment")
-  }
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
 
-  dbURL := os.Getenv("DB_URL")
-  if dbURL == "" {
-    log.Fatal("DB_URL is not found in the environment")
-  }
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
 
-  conn, err := sql.Open("postgres", dbURL)
-  if err != nil {
-    log.Fatal("Can't connect to database")
-  }
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to database")
+	}
 
-  apiCfg := apiConfig {
-    DB: database.New(conn),
-  }
+	apiCfg := apiConfig{
+		DB: database.New(conn),
+	}
 
-  router := chi.NewRouter()
+	router := chi.NewRouter()
 
-  router.Use(cors.Handler(cors.Options {
-    AllowedOrigins: []string{"https://*", "http://*"},
-    AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-    AllowedHeaders: []string{"*"},
-    ExposedHeaders: []string{"Link"},
-    AllowCredentials: false,
-    MaxAge: 300,
-  }))
-  
-  v1Router := chi.NewRouter()
-  v1Router.Get("/healthz", handlerReadiness)
-  v1Router.Get("/err", handlerErr)
-  v1Router.Post("/users", apiCfg.handlerCreateUser)
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 
-  router.Mount("/v1", v1Router)
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.hanlderGetUser))
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
-  log.Printf("Server starting on port %v", portString)
-  server := &http.Server{
-    Handler: router,
-    Addr: ":" + portString,
-  }
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
 
-  err = server.ListenAndServe()
-  if err != nil {
-    log.Fatal(err)
-  }
+	router.Mount("/v1", v1Router)
 
+	log.Printf("Server starting on port %v", portString)
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
-
